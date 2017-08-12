8d258d8c902872a5806bcd278b904c3f6314b5ed
qiqiside <qiqiside@aliyun.com>
2017-08-13 01:57:32 +0800

    add binary search tree.

--- algorithms/common.py
+++ algorithms/common.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 
 def gen_data(num, min=10, max=1000):
@@ -7,4 +8,16 @@ def gen_data(num, min=10, max=1000):
         data.append(random.randint(min, max))
     return data
 
+def shuffle(l):
+    l = clone(l)
+    random.shuffle(l)
+    return l
+
+def sort(l):
+    l = clone(l)
+    l.sort()
+    return l
+
+def clone(o):
+    return copy.deepcopy(o)
 
